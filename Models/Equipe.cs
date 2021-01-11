7e4afa2e7e5314d5584afdedb50ea207eedbc303
unknown <leandroiay3@gmail.com>
2021-01-11 14:09:26 -0300

    erro obj

--- Models/Equipe.cs
+++ Models/Equipe.cs
@@ -2,11 +2,6 @@ namespace EPlayers_AspNetCore.Models
 {
     public class Equipe
     {
-        public int IdEquipe { get; set; }
-
-        public string Nome { get; set; }
-
-        public string Imagem { get; set;}
-
+        
     }
 }
