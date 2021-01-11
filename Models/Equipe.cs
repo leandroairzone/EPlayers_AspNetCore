13c2ee0d5ac1c1546f6f49545a0706763b017fd8
unknown <leandroiay3@gmail.com>
2021-01-11 12:06:10 -0300

    players

---
+++ Models/Equipe.cs
@@ -0,0 +1,12 @@
+namespace EPlayers_AspNetCore.Models
+{
+    public class Equipe
+    {
+        public int IdEquipe { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Imagem { get; set;}
+
+    }
+}
